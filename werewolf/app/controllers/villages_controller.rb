f36f6af79bdc9d324406870d9132c9734dadd2d6
佐々岡哲哉 <st.kyotou1028@gmail.com>
2017-03-12 00:29:23 +0900

    New create の設定

--- werewolf/app/controllers/villages_controller.rb
+++ werewolf/app/controllers/villages_controller.rb
@@ -3,4 +3,18 @@ class VillagesController < ApplicationController
 	def index
 		@villages = Village.all
 	end
+	def new
+		@village = Village.new
+	end
+
+	def create
+		@village = Village.new(village_params)
+		@village.save
+		redirect_to village_path(@village.id)
+	end
+
+	private
+		def village_params
+			params[:village].permit(:name, :describe, :day, :silent, :lock, :code, :number, :job, :secret)
+		end
 end

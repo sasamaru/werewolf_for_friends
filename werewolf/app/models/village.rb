283a6e2e33e05fe2d837d8a501813930fbdf67c0
佐々岡哲哉 <st.kyotou1028@gmail.com>
2017-03-12 00:57:56 +0900

    必須項目の設定

--- werewolf/app/models/village.rb
+++ werewolf/app/models/village.rb
@@ -1,2 +1,7 @@
 class Village < ApplicationRecord
+	#必須項目の設定
+	validates :name,
+	 presence: { message: "名前が入力されてません"},
+	 length: {minimum: 3, message:"3文字以上、20文字以内にしてください"},
+	 length: {maximum: 20, message:"3文字以上、20文字以内にしてください"}
 end

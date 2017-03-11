73acaaa8d64a5576fde31c7b0fb245ad624e7d43
佐々岡哲哉 <st.kyotou1028@gmail.com>
2017-03-11 23:36:37 +0900

    homeの設定

--- werewolf/config/routes.rb
+++ werewolf/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :villages
+
+  #rootが普通にアクセスした時のhomeになる
+  root 'villages#index'
 end
